b90abcd22d38998245497f5628482d7426e68283
Amanda <bell-aman@hotmail.com>
2016-08-31 12:26:26 -0600

    fin

--- controller.js
+++ controller.js
@@ -1,10 +1,28 @@
 angular.module('userProfiles').controller('MainController', function($scope, mainService) {
+$scope.pageNum = 4;
 
-  $scope.getUsers = function() {
-  	$scope.users = mainService.getUsers();
-  }
+$scope.getUsers= function (){
+var promise = mainService.getUsers($scope.pageNum);
 
+  promise.then(function(response){
+    $scope.users = response.data.data;
+    $scope.totalPages = response.data.total_pages;
+})
+}
+$scope.getUsers();
 
+$scope.prev = function(){
+if ($scope.pageNum > 1){
+  $scope.pageNum--;
   $scope.getUsers();
+}
+};
 
-});
+$scope.next = function(){
+if($scope.totalPages > $scope.pageNum){
+  $scope.pageNum++;
+  $scope.getUsers();
+}
+}
+
+  });
